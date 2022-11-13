ecba57a481712433b74548ca2f406a11ba1b05c2
lqh <408285649@qq.com>
2022-11-13 22:04:53 +0800

    更新

--- src/runtime/render.js
+++ src/runtime/render.js
@@ -2,115 +2,111 @@
 import { ShapeFlags } from './vnode';
 import { patchProps } from './patchProps';
 
-
-export function render(vnode,container){
-    const prevVnode = container._vnode;
-    if(!vnode){
-        //n2不存在
-        if(prevVnode){
-            unmount(prevVnode);
-        }
-    }else{
-        patch(prevVnode,vnode,container);
+export function render(vnode, container) {
+  const prevVnode = container._vnode;
+  if (!vnode) {
+    //n2不存在
+    if (prevVnode) {
+      unmount(prevVnode);
     }
+  } else {
+    patch(prevVnode, vnode, container);
+  }
 
-    // mount(vnode,container);
-    container._vnode = vnode;
+  // mount(vnode,container);
+  container._vnode = vnode;
 }
 
-function unmount(vnode){
-    const {shapeFlag,el} = vnode;
-    if(shapeFlag & ShapeFlags.COMPONENT){
-        unmountComponent(vnode);
-    }else if(shapeFlag & ShapeFlags.FRAGMENT){
-        unmountFragment(vnode);
-    }else{
-        el.parentNode.removeChild(el);
-    }
+function unmount(vnode) {
+  const { shapeFlag, el } = vnode;
+  if (shapeFlag & ShapeFlags.COMPONENT) {
+    unmountComponent(vnode);
+  } else if (shapeFlag & ShapeFlags.FRAGMENT) {
+    unmountFragment(vnode);
+  } else {
+    el.parentNode.removeChild(el);
+  }
 }
 
-function unmountComponent(vnode){
-    
+function unmountComponent(vnode) {}
 
-}
-
-function processComponent(n1,n2,container,anchor){
-
-}
+function processComponent(n1, n2, container, anchor) {}
 
 /**
  * 删除Fragment节点（el和anchor）
- * @param {*} vnode 
+ * @param {*} vnode
  */
-function unmountFragment(vnode){
-    let {el:cur,anchor:end} = vnode;
-    const {parentNode} = cur;
+function unmountFragment(vnode) {
+  let { el: cur, anchor: end } = vnode;
+  const { parentNode } = cur;
 
-    while(cur !== end){
-        let next = cur.nextSibling;
-        parentNode.removeChild(cur);
-        cur = next;
-    }
+  while (cur !== end) {
+    let next = cur.nextSibling;
+    parentNode.removeChild(cur);
+    cur = next;
+  }
 
-    parentNode.removeChild(end);
+  parentNode.removeChild(end);
 }
 
-function patch(n1,n2,container,anchor){
-    if(n1 && !isSameVNode(n1,n2)){
-        anchor =(n1.anchor||n1.el).nextSibling;
-        unmount(n1);
-        n1 = null;
-    }
+function patch(n1, n2, container, anchor) {
+  if (n1 && !isSameVNode(n1, n2)) {
+    anchor = (n1.anchor || n1.el).nextSibling;
+    unmount(n1);
+    n1 = null;
+  }
 
-    const {shapeFlag} = n2;
-    if(shapeFlag & ShapeFlags.COMPONENT){
-        processComponent(n1,n2,container,anchor);
-    }else if(shapeFlag & ShapeFlags.TEXT){
-        processText(n1,n2,container,anchor);
-    }else if(shapeFlag & ShapeFlags.FRAGMENT){
-        processFragment(n1,n2,container,anchor)
-    }else{
-        processElement(n1,n2,container,anchor)
-    }
+  const { shapeFlag } = n2;
+  if (shapeFlag & ShapeFlags.COMPONENT) {
+    processComponent(n1, n2, container, anchor);
+  } else if (shapeFlag & ShapeFlags.TEXT) {
+    processText(n1, n2, container, anchor);
+  } else if (shapeFlag & ShapeFlags.FRAGMENT) {
+    processFragment(n1, n2, container, anchor);
+  } else {
+    processElement(n1, n2, container, anchor);
+  }
 }
 
-function isSameVNode(n1,n2){
-    return n1.type === n2.type;
+function isSameVNode(n1, n2) {
+  return n1.type === n2.type;
 }
 
-function processText(n1,n2,container,anchor){
-    if(n1){
-        n2.el =  n1.el;
-        n1.el.textContent = n2.children;
-    }else{
-        mountTextNode(n2,container,anchor)
-    }
+function processText(n1, n2, container, anchor) {
+  if (n1) {
+    n2.el = n1.el;
+    n1.el.textContent = n2.children;
+  } else {
+    mountTextNode(n2, container, anchor);
+  }
 }
 
-function processFragment(n1,n2,container,anchor){
-    //anchor 创建空节点,解决append el位置错乱问题
-    // const fragmentStartAnchor = document.createTextNode('');
-    // const fragmentEndAnchor = document.createTextNode('');
-    const fragmentStartAnchor = n2.el = n1?n1.el:document.createTextNode('');
-    const fragmentEndAnchor = n2.el = n1?n1.el:document.createTextNode('');
-    n2.anchor = n1?n1.anchor:fragmentEndAnchor;
-    if(n1){
-        patchChildren(n1,n2,container,fragmentEndAnchor)
-    }else{
-        // container.appendChild(fragmentStartAnchor)
-        // container.appendChild(fragmentEndAnchor)
-        container.insertBefore(fragmentStartAnchor,anchor);
-        container.insertBefore(fragmentEndAnchor,anchor);
-        mountChildren(n2.children,container,fragmentEndAnchor);
-    }
+function processFragment(n1, n2, container, anchor) {
+  //anchor 创建空节点,解决append el位置错乱问题
+  // const fragmentStartAnchor = document.createTextNode('');
+  // const fragmentEndAnchor = document.createTextNode('');
+  const fragmentStartAnchor = (n2.el = n1
+    ? n1.el
+    : document.createTextNode(''));
+  const fragmentEndAnchor = (n2.el = n1 ? n1.el : document.createTextNode(''));
+  n2.anchor = n1 ? n1.anchor : fragmentEndAnchor;
+  if (n1) {
+    patchChildren(n1, n2, container, fragmentEndAnchor);
+  } else {
+    // container.appendChild(fragmentStartAnchor)
+    // container.appendChild(fragmentEndAnchor)
+    container.insertBefore(fragmentStartAnchor, anchor);
+    container.insertBefore(fragmentEndAnchor, anchor);
+    mountChildren(n2.children, container, fragmentEndAnchor);
+  }
 }
 
-function processElement(n1,n2,container,anchor){
-    if(n1){
-        patchElement(n1,n2);
-    }else{
-        mountElement(n2,container,anchor);
-    }
+function processElement(n1, n2, container, anchor) {
+  if (n1) {
+    patchElement(n1, n2);
+  } else {
+    mountElement(n2, container, anchor);
+  }
 }
 
 /**
@@ -124,15 +120,14 @@ function processElement(n1,n2,container,anchor){
  * @param {*} vnode
  * @param {*} container
  */
- function mountTextNode(vnode, container,anchor) {
-    const textNode = document.createTextNode(vnode.children);
-    // container.appendChild(textNode);
-    container.insertBefore(textNode,anchor)
-    vnode.el = textNode;
-  }
+function mountTextNode(vnode, container, anchor) {
+  const textNode = document.createTextNode(vnode.children);
+  // container.appendChild(textNode);
+  container.insertBefore(textNode, anchor);
+  vnode.el = textNode;
+}
 
-
-  /**
+/**
  * element普通元素，使用document.createElement创建。type:类型，props属性，children子元素(字符串或数组)
  * 例如：
  * {
@@ -143,30 +138,30 @@ function processElement(n1,n2,container,anchor){
  * @param {*} vnode
  * @param {*} container
  */
-function mountElement(vnode, container,anchor) {
-    const { type, props, shapeFlag,children } = vnode;
-    const el = document.createElement(type);
-    // mountProps(props, el);
-    patchProps(el,null,props);
-    // mountChildren(vnode, el);
-    
-    // const { shapeFlag, children } = vnode;
-    if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
-      mountTextNode(vnode, el);
-    } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
-     mountChildren(children,el)
-    }
-    // container.appendChild(el);
-    container.insertBefore(el,anchor)
-    vnode.el = el;
-  }
+function mountElement(vnode, container, anchor) {
+  const { type, props, shapeFlag, children } = vnode;
+  const el = document.createElement(type);
+  // mountProps(props, el);
+  patchProps(el, null, props);
+  // mountChildren(vnode, el);
 
-  function mountChildren(children, container,anchor) {
-    children.forEach((child) => {
-        // mount(child, container);
-        patch(null,child,container,anchor);
-      });
+  // const { shapeFlag, children } = vnode;
+  if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
+    mountTextNode(vnode, el);
+  } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+    mountChildren(children, el);
   }
+  // container.appendChild(el);
+  container.insertBefore(el, anchor);
+  vnode.el = el;
+}
+
+function mountChildren(children, container, anchor) {
+  children.forEach((child) => {
+    // mount(child, container);
+    patch(null, child, container, anchor);
+  });
+}
 
 // function mountProps(props, el) {
 //   for (const key in props) {
@@ -204,128 +199,140 @@ function mountElement(vnode, container,anchor) {
 //   }
 // }
 
-function patchElement(n1,n2){
-    //继承n1
-    n2.el = n1.el;
-    patchProps(n2.el, n1.props, n2.props);
-    patchChildren(n1,n2,n2.el)
+function patchElement(n1, n2) {
+  //继承n1
+  n2.el = n1.el;
+  patchProps(n2.el, n1.props, n2.props);
+  patchChildren(n1, n2, n2.el);
 }
 
-
-
-function unmounChildren(children){
-    children.forEach((child)=>{
-        unmount(child);
-    })
+function unmounChildren(children) {
+  children.forEach((child) => {
+    unmount(child);
+  });
 }
 
 function patchChildren(n1, n2, container, anchor) {
-    const { shapeFlag: prevShapeFlag, children: c1 } = n1;
-    const { shapeFlag, children: c2 } = n2;
-  
-    //九种情况的判断--对应图解图片文件
-    if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
-      if (prevShapeFlag & ShapeFlags.ARRAY_CHILDREN) {
-        unmountChildren(c1);
-      }
-      if (c2 !== c1) {
-        container.textContent = c2;
-      }
-    } else {
-      
-      if (prevShapeFlag & ShapeFlags.ARRAY_CHILDREN) {
-        
-        if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
-          
-          
-          if (c1[0] && c1[0].key != null && c2[0] && c2[0].key != null) {
-            patchKeyedChildren(c1, c2, container, anchor);
-          } else {
-            patchUnkeyedChildren(c1, c2, container, anchor);
-          }
+  const { shapeFlag: prevShapeFlag, children: c1 } = n1;
+  const { shapeFlag, children: c2 } = n2;
+
+  //九种情况的判断--对应图解图片文件
+  if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
+    if (prevShapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+      unmountChildren(c1);
+    }
+    if (c2 !== c1) {
+      container.textContent = c2;
+    }
+  } else {
+    if (prevShapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+      if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+        if (c1[0] && c1[0].key != null && c2[0] && c2[0].key != null) {
+          patchKeyedChildren(c1, c2, container, anchor);
         } else {
-         
-          unmountChildren(c1);
+          patchUnkeyedChildren(c1, c2, container, anchor);
         }
       } else {
-        
-        if (prevShapeFlag & ShapeFlags.TEXT_CHILDREN) {
-          container.textContent = '';
-        }
-        if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
-          mountChildren(c2, container, anchor);
-        }
+        unmountChildren(c1);
+      }
+    } else {
+      if (prevShapeFlag & ShapeFlags.TEXT_CHILDREN) {
+        container.textContent = '';
+      }
+      if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+        mountChildren(c2, container, anchor);
       }
     }
   }
-
-  function patchUnkeyedChildren(c1, c2, container, anchor) {
-    const oldLength = c1.length;
-    const newLength = c2.length;
-    const commonLength = Math.min(oldLength, newLength);
-    for (let i = 0; i < commonLength; i++) {
-      patch(c1[i], c2[i], container, anchor);
-    }
-    if (newLength > oldLength) {
-      mountChildren(c2.slice(commonLength), container, anchor);
-    } else if (newLength < oldLength) {
-      unmountChildren(c1.slice(commonLength));
-    }
-  }
-function patchChildren2(n1,n2,container){
-    const {shapeFlag:prevShapeFlag, children:c1} = n1;
-    const {shapeFlag,children:c2} = n2;
-    if(shapeFlag & ShapeFlags.TEXT_CHILDREN){
-
-       if(prevShapeFlag & ShapeFlags.ARRAY_CHILDREN){
-        unmounChildren(c1);
-       }
-       if(c1!==c2){
-        //更新
-        container.textContent = c2;
-       }
-        
-    }else if(shapeFlag & ShapeFlags.ARRAY_CHILDREN){
-
-        if(prevShapeFlag & ShapeFlags.TEXT_CHILDREN){
-            container.textContent = '';
-            mountChildren(c2,container,anchor);
-        }else if(prevShapeFlag & ShapeFlags.ARRAY_CHILDREN){
-            patchArrayChildren(c1,c2,container,anchor);
-        }else{
-            mountChildren(c2,container,anchor);
-         }
-    }else{
-
-        if(prevShapeFlag & ShapeFlags.TEXT_CHILDREN){
-            container.textContent = '';
-
-        }else if(prevShapeFlag & ShapeFlags.ARRAY_CHILDREN){
-            unmounChildren(c1);
-    
-        }else{
-    
-        }
-
-    }
 }
 
-function patchArrayChildren(c1,c2,container,anchor){
-    const oldLength = c1.length;
-    const newLength = c2.length;
+function patchUnkeyedChildren(c1, c2, container, anchor) {
+  const oldLength = c1.length;
+  const newLength = c2.length;
+  const commonLength = Math.min(oldLength, newLength);
+  for (let i = 0; i < commonLength; i++) {
+    patch(c1[i], c2[i], container, anchor);
+  }
+  if (newLength > oldLength) {
+    mountChildren(c2.slice(commonLength), container, anchor);
+  } else if (newLength < oldLength) {
+    unmountChildren(c1.slice(commonLength));
+  }
+}
 
-    const commonLength = Math.min(oldLength,newLength);
-    for (let i = 0; i < commonLength; i++) {
-        patch(c1[i],c2[i],container,anchor);
+/**
+ * 通过key来对比节点更新
+ * @param {*} c1
+ * @param {*} c2
+ * @param {*} container
+ * @param {*} anchor
+ */
+function patchKeyedChildren(c1, c2, container, anchor) {
+  for (let i = 0; i < c2.length; i++) {
+    const next = c2[i];
+    for (let j = 0; j < c1.length; j++) {
+      const prev = c1[i];
+      if (next.key === prev.key) {
+        patch(prev, next, container, anchor);
+        // const curAnchor = c1[0].el;
+        const curAnchor = c2[i - 1].el.nextSibling;
+        //TODO
+        // 若干el为新节点执行插入操作，如果已经存在的节点，执行移动操作
+        container.insertBefore(next.el, anchor);
+        break;
+      }
     }
-
-    if(oldLength>newLength){
-        unmounChildren(c1.slice(commonLength));
-
-    }else if(oldLength>newLength){
-        mountChildren(c2.slice(commonLength),container,anchor);
-        
+  }
+}
+function patchChildren2(n1, n2, container) {
+  const { shapeFlag: prevShapeFlag, children: c1 } = n1;
+  const { shapeFlag, children: c2 } = n2;
+  if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
+    if (prevShapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+      unmounChildren(c1);
     }
+    if (c1 !== c2) {
+      //更新
+      container.textContent = c2;
+    }
+  } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+    if (prevShapeFlag & ShapeFlags.TEXT_CHILDREN) {
+      container.textContent = '';
+      mountChildren(c2, container, anchor);
+    } else if (prevShapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+      //只要第一个元素有key那么就当作都有key
+      if (c1[0] && c1[0].key !== null && c2[0].key !== null) {
+        patchUnkeyedChildren(c1, c2, container, anchor);
+      } else {
+        patchUnkeyedChildren(c1, c2, container, anchor);
+      }
 
+      // patchArrayChildren(c1,c2,container,anchor);
+    } else {
+      mountChildren(c2, container, anchor);
+    }
+  } else {
+    if (prevShapeFlag & ShapeFlags.TEXT_CHILDREN) {
+      container.textContent = '';
+    } else if (prevShapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+      unmounChildren(c1);
+    } else {
+    }
+  }
+}
 
+function patchArrayChildren(c1, c2, container, anchor) {
+  const oldLength = c1.length;
+  const newLength = c2.length;
+
+  const commonLength = Math.min(oldLength, newLength);
+  for (let i = 0; i < commonLength; i++) {
+    patch(c1[i], c2[i], container, anchor);
+  }
+
+  if (oldLength > newLength) {
+    unmounChildren(c1.slice(commonLength));
+  } else if (oldLength > newLength) {
+    mountChildren(c2.slice(commonLength), container, anchor);
+  }
 }
